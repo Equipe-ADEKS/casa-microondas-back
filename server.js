a271c4f322a7be3c744bd835697a41ee454a591b
EmmanuelJuca <emmanueljucca@gmail.com>
2024-06-05 21:51:26 -0300

    modificado

--- server.js
+++ server.js
@@ -5,27 +5,27 @@ import mysql from "mysql2";
 const app = express();
 const porta = 3000;
 const conexao = mysql.createConnection({
-  host:"localhost",
-  port:3306,
+  host: "localhost",
+  port: 3306,
   database: "casa_microondas_login",
-  user:"root"
+  user: "root"
 });
 
 conexao.connect();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(porta, () => {
   console.log("servidor rodando e escutando na porta 3000");
 });
 
 app.get("/", (req, res) => {
-    res.status(200).send( "Nosso servidor da CMO" );
+  res.status(200).send("Nosso servidor da CMO");
 });
 
 app.get("/servicos", (req, res) => {
-    res.status(200).send("rota para trazer os servicos ");
+  res.status(200).send("rota para trazer os servicos ");
 });
 
 let html = '';
@@ -46,8 +46,8 @@ app.get("/marcas", (req, res) => {
       <p>Breve descrição</p>
     </body>
       </html>`;
-    res.status(200).send(html);
-  });
+  res.status(200).send(html);
+});
 
 
 //Rotas para inclusão de novos serviços
@@ -61,9 +61,9 @@ app.post("/servicos", (req, res) => {
   let ativo = true;
 
   conexao.query(
-    `call sp_ins_servico (@id, ?, ?, ?, ?, ?, ?, @mensagem)`,
+    `call Sp_Ins_Servico (@id, ?, ?, ?, ?, ?, ?, @mensagem)`,
     [tit, desc, img, ordem, url, ativo], (erro, linhas) => {
-      if(erro) {
+      if (erro) {
         console.log(erro);
         res.send('Problema ao inserir serviço')
       }
@@ -73,6 +73,27 @@ app.post("/servicos", (req, res) => {
       }
     });
 });
+
+app.post("/marca", (req, res) => {
+  let desc = req.body.descricao;
+  let url_marca = req.body.url_marca;
+  let logo = req.body.logo;
+  let flag = true;
+
+  conexao.query(
+    `call SP_Ins_Marca (@id, ?, ?, ?, ?, @mensagem)`,
+    [desc, url_marca, logo, flag], (erro, linhas) => {
+      if (erro) {
+        console.log(erro);
+        res.send('Problema ao inserir serviço')
+      }
+      else {
+        console.log(linhas);
+        res.send('Serviço inserido');
+      }
+    });
+});
+
 
 //Servidor criado com js puro
 //import http from "http";
